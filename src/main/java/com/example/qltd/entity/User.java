b5e82b74a3a3358debdc0880f074553d84e7a4eb
Thanh Cong <duongthanhcong9a.1617@gmail.com>
2026-07-06 20:07:05 +0700

    fix : status default

--- src/main/java/com/example/qltd/entity/User.java
+++ src/main/java/com/example/qltd/entity/User.java
@@ -46,6 +46,7 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
+    @Builder.Default
     private UserStatus status = UserStatus.ACTIVE;
 
     @Column(name = "created_at", nullable = false, updatable = false)
