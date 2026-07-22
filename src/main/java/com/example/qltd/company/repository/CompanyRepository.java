7babc26797bff7ea977764316cffb6ddb69bcc67
Thanh Cong <duongthanhcong9a.1617@gmail.com>
2026-07-22 20:37:15 +0700

    feat(company): implement company repository

--- src/main/java/com/example/qltd/company/repository/CompanyRepository.java
+++ src/main/java/com/example/qltd/company/repository/CompanyRepository.java
@@ -5,8 +5,10 @@ import com.example.qltd.company.entity.Company;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface CompanyRepository extends JpaRepository <Company, Long> {
+public interface CompanyRepository extends JpaRepository <Company, Long>,
+        JpaSpecificationExecutor<Company> {
 
     Optional<Company> findBySlug(String slug);
 
